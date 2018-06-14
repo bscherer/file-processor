41f48d5bd952a60425f9e168d7b4e3ca0b85f593
Bruno Scherer Ferreira <brunoschererferreira@gmail.com>
2018-06-14 03:32:00 -0300

    Primeira implementação da criação de um Client através de um registro

--- src/main/java/com/mycompany/fileprocessor/domain/factories/ClientFactory.java
+++ src/main/java/com/mycompany/fileprocessor/domain/factories/ClientFactory.java
@@ -1,12 +1,14 @@
 package com.mycompany.fileprocessor.domain.factories;
 
+import com.mycompany.fileprocessor.domain.entity.Client;
 import com.mycompany.fileprocessor.domain.entity.Entity;
 
 public class ClientFactory implements EntityFactory {
 
     @Override
     public Entity create(String registry) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String[] splitedRegistries = registry.split("ç");
+        return new Client(splitedRegistries[1], splitedRegistries[2], splitedRegistries[3]);
     }
     
 }
