80854ebead6c784d2b08efbfa51d3a98c994e2b0
Bruno Scherer Ferreira <brunoschererferreira@gmail.com>
2018-06-14 06:11:09 -0300

    Adicionada implementação básica da SaleFactory

--- src/main/java/com/mycompany/fileprocessor/domain/factories/SaleFactory.java
+++ src/main/java/com/mycompany/fileprocessor/domain/factories/SaleFactory.java
@@ -1,12 +1,33 @@
 package com.mycompany.fileprocessor.domain.factories;
 
 import com.mycompany.fileprocessor.domain.entity.Entity;
+import com.mycompany.fileprocessor.domain.entity.Sale;
+import com.mycompany.fileprocessor.domain.entity.SaleItem;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class SaleFactory implements EntityFactory {
 
     @Override
     public Entity create(String registry) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String[] splitedRegistries = registry.split("ç");
+        Sale sale = new Sale(
+                new Long(splitedRegistries[1]),
+                createSaleItems(splitedRegistries[2]),
+                splitedRegistries[3]        
+        );        
+        return sale;
+    }
+    
+    private List<SaleItem> createSaleItems(String itemsRegistry){
+        List<SaleItem> saleItems = new ArrayList();
+        List<String> items = Arrays.asList(itemsRegistry.replace("[", "").replace("]", "").split(","));
+        EntityFactory saleItemsFactory = new SaleItemFactory();
+        for(String item : items) {
+            saleItems.add((SaleItem) saleItemsFactory.create(item));
+        }
+        return saleItems;
     }
     
 }
